ded040c649b56f789fe498b49ff321fa13beb65b
Ali Kaif <aliakaif@email.arizona.edu>
2019-01-28 15:53:51 -0700

    Add files via upload

---
+++ stalkerScript.js
@@ -0,0 +1,10 @@
+var userName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+
+console.log("The user's full name is " + userName + " " + 
+    lastName);
+
+console.log("The user is " + userAge + " years old")
+
+
